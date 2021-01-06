3db603231ab7244610f1ce6e009f3bb85242c107
VikaSol1 <75083339+VikaSol1@users.noreply.github.com>
2021-01-07 00:57:37 +0200

    neweb

---
+++ website/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+import datetime
+from django.db import models
+
+# from profiles.models import Profile
+
+# Create your models here.
+
+
+class Timestamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Event(Timestamped):
+    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    objects = None
+    date = models.DateTimeField(default=datetime.datetime.now)
+    # Passport = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    # last_name = models.CharField(max_length=20)
+    # phone_number = models.CharField(max_length=50)
+
+    class Meta:
+        default_related_name = 'events'
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name+' '
+
+class DocumentationPA(Timestamped):
+
+
+    class Meta:
+        default_related_name = 'events'
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name+' '
---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('Appointment', views.Appointment, name="Appointment"),
+path('documentation', views.documentation, name="documentation"),
+]
