2c7b78fb3e7582153f028a33db60698565bb5e57
VikaSol1 <75083339+VikaSol1@users.noreply.github.com>
2021-01-07 23:53:04 +0200

    new home,base

--- website/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from.models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['name','date']
-        # 'date''last_name','phone_number
--- website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from.import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('Appointment', views.Appointment, name="Appointment"),
-path('documentation', views.documentation, name="documentation"),
-]
